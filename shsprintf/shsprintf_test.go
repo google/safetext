c62632a6115a82f76511d283dc019d6cb2709460
RelunSec <cs7778503@gmail.com>
2026-03-27 03:37:34 -0700

    Revert existing comments

--- shsprintf/shsprintf_test.go
+++ shsprintf/shsprintf_test.go
@@ -6,7 +6,7 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing,
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -38,20 +38,25 @@ func TestSafetextShsprintf(t *testing.T) {
             },
             err: nil,
         },
+
         {
             format: "echo %s",
             replacements: []any{
+                // '+' is allowed
                 "Just+A+Random+String",
             },
             err: nil,
         },
+
         {
             format: "echo %s",
             replacements: []any{
+                // '@' is allowed
                 "like@an.email",
             },
             err: nil,
         },
+
         {
             format: `echo "%s"`,
             replacements: []any{
@@ -59,6 +64,7 @@ func TestSafetextShsprintf(t *testing.T) {
             },
             err: nil,
         },
+
         {
             format: `echo '%s''s '`,
             replacements: []any{
@@ -66,6 +72,7 @@ func TestSafetextShsprintf(t *testing.T) {
             },
             err: nil,
         },
+
         {
             format: `command "x-%s-y-%s"`,
             replacements: []any{
@@ -74,6 +81,8 @@ func TestSafetextShsprintf(t *testing.T) {
             },
             err: nil,
         },
+
+        // Truncation
         {
             format: `command "%.4s"`,
             replacements: []any{
@@ -81,6 +90,7 @@ func TestSafetextShsprintf(t *testing.T) {
             },
             err: nil,
         },
+
         {
             format: `ls %s`,
             replacements: []any{
@@ -88,6 +98,7 @@ func TestSafetextShsprintf(t *testing.T) {
             },
             err: nil,
         },
+
         {
             format: "echo `cat %s`",
             replacements: []any{
@@ -95,6 +106,7 @@ func TestSafetextShsprintf(t *testing.T) {
             },
             err: nil,
         },
+
         {
             format: `#! /bin/bash
 end=$((SECONDS+%d))
@@ -106,6 +118,7 @@ done`,
             },
             err: nil,
         },
+
         {
             format: `echo "$(( %s + %s ))"`,
             replacements: []any{
@@ -114,6 +127,8 @@ done`,
             },
             err: nil,
         },
+
+        // String values in items in loops can change (as long as new expressions not introduced)
         {
             format: `for VARIABLE in file1 %s file3
 do
@@ -124,6 +139,8 @@ done`,
             },
             err: nil,
         },
+
+        // C-style loop
         {
             format: `for (( c=1; c<=%s; c++ ))
 do
@@ -134,6 +151,8 @@ done`,
             },
             err: nil,
         },
+
+        // If statement conditions fine to change
         {
             format: `if [ %s ] ; then
 command
@@ -143,6 +162,8 @@ fi`,
             },
             err: nil,
         },
+
+        // Extended if
         {
             format: `
 if [[ -e %s ]]
@@ -157,6 +178,7 @@ fi`,
 
         // Positive cases
         // ====================================================
+        // Command injection
         {
             format: "echo %s",
             replacements: []any{
@@ -164,27 +186,36 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Special characters
         {
             format: "echo %s",
             replacements: []any{
+                // globbing chars ('?') are not allowed
                 "Just?ARandomString",
             },
             err: shsprintf.ErrShInjection,
         },
+
         {
             format: "echo %s",
             replacements: []any{
+                // globbing chars ('*') are not allowed
                 "Just*ARandomString",
             },
             err: shsprintf.ErrShInjection,
         },
+
         {
             format: "echo %s",
             replacements: []any{
+                // ! is command history
                 "Just!ARandomString",
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Multiple arguments
         {
             format: "cat %s",
             replacements: []any{
@@ -192,6 +223,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Flags
         {
             format: "cat %s",
             replacements: []any{
@@ -199,6 +232,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Condition with new expressions
         {
             format: `
 if [[ -e %s ]]
@@ -210,6 +245,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Command injection 2
         {
             format: "echo %s",
             replacements: []any{
@@ -217,6 +254,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Command injection 3
         {
             format: "echo %s",
             replacements: []any{
@@ -224,6 +263,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Command injection 4
         {
             format: "echo %s foobar",
             replacements: []any{
@@ -231,6 +272,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Command injection 5
         {
             format: "echo %s",
             replacements: []any{
@@ -238,6 +281,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Command injection 6
         {
             format: "echo %s",
             replacements: []any{
@@ -245,6 +290,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Command injection 7
         {
             format: "echo %s",
             replacements: []any{
@@ -252,6 +299,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Command injection 7 - cross check
         {
             format: "echo %s",
             replacements: []any{
@@ -259,6 +308,8 @@ fi`,
             },
             err: nil,
         },
+
+        // Command injection 8
         {
             format: `echo "%s"`,
             replacements: []any{
@@ -266,6 +317,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Command injection 9
         {
             format: `echo '%s'`,
             replacements: []any{
@@ -273,6 +326,8 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // Argument injection due to glob operator in literal strings
         {
             format: `touch %s; echo %s`,
             replacements: []any{
@@ -281,6 +336,354 @@ fi`,
             },
             err: shsprintf.ErrShInjection,
         },
+
+        // ANSI-C style
+        {
+            format: `command %s`,
+            replacements: []any{
+                `$'\u002d\u002d'flag=value`,
+            },
+            err: shsprintf.ErrShInjection,
+        },// Copyright 2024 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package shsprintf_test
+
+import (
+    "strings"
+    "testing"
+
+    "github.com/google/safetext/shsprintf"
+)
+
+func TestSafetextShsprintf(t *testing.T) {
+    type testCase struct {
+        format       string
+        replacements []any
+        err          error
+    }
+
+    testCases := []testCase{
+        // Negative cases
+        // ====================================================
+        {
+            format: "echo %s",
+            replacements: []any{
+                "hello",
+            },
+            err: nil,
+        },
+
+        {
+            format: "echo %s",
+            replacements: []any{
+                // '+' is allowed
+                "Just+A+Random+String",
+            },
+            err: nil,
+        },
+
+        {
+            format: "echo %s",
+            replacements: []any{
+                // '@' is allowed
+                "like@an.email",
+            },
+            err: nil,
+        },
+
+        {
+            format: `echo "%s"`,
+            replacements: []any{
+                "hello hello",
+            },
+            err: nil,
+        },
+
+        {
+            format: `echo '%s''s '`,
+            replacements: []any{
+                "blas hello",
+            },
+            err: nil,
+        },
+
+        {
+            format: `command "x-%s-y-%s"`,
+            replacements: []any{
+                "ar\\\"g-yz",
+                "bla",
+            },
+            err: nil,
+        },
+
+        // Truncation
+        {
+            format: `command "%.4s"`,
+            replacements: []any{
+                "bla",
+            },
+            err: nil,
+        },
+
+        {
+            format: `ls %s`,
+            replacements: []any{
+                "/tmp" + "/bla",
+            },
+            err: nil,
+        },
+
+        {
+            format: "echo `cat %s`",
+            replacements: []any{
+                "hello",
+            },
+            err: nil,
+        },
+
+        {
+            format: `#! /bin/bash
+end=$((SECONDS+%d))
+
+while [ $SECONDS -lt $end ]; do
+done`,
+            replacements: []any{
+                3,
+            },
+            err: nil,
+        },
+
+        {
+            format: `echo "$(( %s + %s ))"`,
+            replacements: []any{
+                "12",
+                "10",
+            },
+            err: nil,
+        },
+
+        // String values in items in loops can change (as long as new expressions not introduced)
+        {
+            format: `for VARIABLE in file1 %s file3
+do
+    cat $VARIABLE
+done`,
+            replacements: []any{
+                "bla",
+            },
+            err: nil,
+        },
+
+        // C-style loop
+        {
+            format: `for (( c=1; c<=%s; c++ ))
+do
+  shell_COMMANDS
+done`,
+            replacements: []any{
+                "5",
+            },
+            err: nil,
+        },
+
+        // If statement conditions fine to change
+        {
+            format: `if [ %s ] ; then
+command
+fi`,
+            replacements: []any{
+                "condition",
+            },
+            err: nil,
+        },
+
+        // Extended if
+        {
+            format: `
+if [[ -e %s ]]
+then
+  echo "File exists"
+fi`,
+            replacements: []any{
+                "file",
+            },
+            err: nil,
+        },
+
+        // Positive cases
+        // ====================================================
+        // Command injection
+        {
+            format: "echo %s",
+            replacements: []any{
+                "`./command`",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Special characters
+        {
+            format: "echo %s",
+            replacements: []any{
+                // globbing chars ('?') are not allowed
+                "Just?ARandomString",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        {
+            format: "echo %s",
+            replacements: []any{
+                // globbing chars ('*') are not allowed
+                "Just*ARandomString",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        {
+            format: "echo %s",
+            replacements: []any{
+                // ! is command history
+                "Just!ARandomString",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Multiple arguments
+        {
+            format: "cat %s",
+            replacements: []any{
+                "one two three",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Flags
+        {
+            format: "cat %s",
+            replacements: []any{
+                "-flag",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Condition with new expressions
+        {
+            format: `
+if [[ -e %s ]]
+then
+  echo "File exists"
+fi`,
+            replacements: []any{
+                "file || (1==1)",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Command injection 2
+        {
+            format: "echo %s",
+            replacements: []any{
+                "$(./command)",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Command injection 3
+        {
+            format: "echo %s",
+            replacements: []any{
+                "foobar\ncommand",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Command injection 4
+        {
+            format: "echo %s foobar",
+            replacements: []any{
+                ";",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Command injection 5
+        {
+            format: "echo %s",
+            replacements: []any{
+                "foo$(./command)bar",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Command injection 6
+        {
+            format: "echo %s",
+            replacements: []any{
+                "foo`./command`bar",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Command injection 7
+        {
+            format: "echo %s",
+            replacements: []any{
+                `"foo$(./command)bar"`,
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Command injection 7 - cross check
+        {
+            format: "echo %s",
+            replacements: []any{
+                `foo/commandbar`,
+            },
+            err: nil,
+        },
+
+        // Command injection 8
+        {
+            format: `echo "%s"`,
+            replacements: []any{
+                `foo$(./command)bar`,
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Command injection 9
+        {
+            format: `echo '%s'`,
+            replacements: []any{
+                `foo' ./command 'bar`,
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // Argument injection due to glob operator in literal strings
+        {
+            format: `touch %s; echo %s`,
+            replacements: []any{
+                "./--some-param=value",
+                "*value",
+            },
+            err: shsprintf.ErrShInjection,
+        },
+
+        // ANSI-C style
         {
             format: `command %s`,
             replacements: []any{
